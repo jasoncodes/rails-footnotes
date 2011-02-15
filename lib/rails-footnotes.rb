6d4fa256f5e7839e5b90007c38251ad18fbb1090
Andre Arko <andre@arko.net>
2011-02-14 19:36:27 -0800

    only activate railtie in development mode

--- lib/rails-footnotes.rb
+++ lib/rails-footnotes.rb
@@ -1,18 +1,20 @@
 module Footnotes
 
-  # The footnotes are applied by default to all actions. To remove the
-  # footnotes from an action, use skip_filter in your controller.
   class Railtie < ::Rails::Railtie
     config.before_configuration do
-      require 'rails-footnotes/footnotes'
-      require 'rails-footnotes/backtracer'
+      if ::Rails.env.development?
+        require 'rails-footnotes/footnotes'
+        require 'rails-footnotes/backtracer'
 
-      # Require each individual note
-      notes_glob = File.expand_path("../rails-footnotes/notes/*.rb", __FILE__)
-      Dir[notes_glob].each{|note| require note }
+        # Require each individual note
+        notes_glob = File.expand_path("../rails-footnotes/notes/*.rb", __FILE__)
+        Dir[notes_glob].each{|note| require note }
 
-      ActionController::Base.prepend_before_filter Footnotes::BeforeFilter
-      ActionController::Base.after_filter Footnotes::AfterFilter
+        # The footnotes are applied by default to all actions. To remove the
+        # footnotes from an action, use skip_filter in your controller.
+        ActionController::Base.prepend_before_filter Footnotes::BeforeFilter
+        ActionController::Base.after_filter Footnotes::AfterFilter
+      end
     end
   end
 
